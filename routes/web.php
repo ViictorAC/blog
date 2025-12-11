9f626a63c44e255308946549918aa9158996fa4f
Viictor_AC <victorach007@gmail.com>
2025-12-11 10:27:18 +0100

    Commit final Ejercicio 1 Sesion 2

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('inicio');
 
 Route::get('/posts', function () {
     return "Listado de posts";
-});
+})->name('posts_listado');
+
+Route::get('/posts/{id}', function ($id) {
+    return "Ficha del post $id";
+})->where('id', '[0-9]+') ->name('posts_ficha');
