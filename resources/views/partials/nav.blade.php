83cb042e5fa974fbee6831e22f76718df2b314fe
Viictor_AC <victorach007@gmail.com>
2025-12-15 12:46:19 +0100

    Final sessio 2 Ejercici 3

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,5 +1,19 @@
-    <nav>
-        <a href="{{ route('inicio') }}">Inicio</a>
-        &nbsp;&nbsp;
-        <a href="{{ route('posts_listado') }}">Listado de posts</a>
-    </nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <a class="navbar-brand" href="#">Blog
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+        aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="{{ route('inicio') }}">Inicio <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('posts_listado') }}">Listado de posts</a>
+            </li>
+        </ul>
+    </div>
+    </div>
+</nav>
